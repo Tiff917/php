2864e978f000bc9bb1f8a5997978bd43e5e0558f
Tiffany <yxiao852@gmail.com>
2026-03-07 15:06:33 +0800

    第二次嘗試上傳

---
+++ A1123382.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo '蕭語婕<br>';
+echo 'Hello World!<br>';
+
+echo '<br>';
+
+$i=20;
+$j=$i+10;
+
+echo "My year is $i .<br>";
+echo "After ten years,my year is $j .<br>";
+
+
+?>
+
+</body>
+</html>
